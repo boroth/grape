8c1d4515acf964c01dd39ba1f88705ad972080a6
Bo Roth <bo@leadingreach.com>
2016-10-27 15:31:06 -0500

    Add cookie-parser and express-session for session authentication

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,6 +1,13 @@
 'use strict';
 
+var loopback = require('loopback');
+
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+
+  server.middleware('auth', loopback.token({
+    model: server.models.accessToken,
+    currentUserLiteral: 'me'
+  }));
 };
